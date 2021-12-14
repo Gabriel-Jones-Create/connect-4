656dc6b8cbb846c1ccf6ff7052a3623b18145edd
Gabriel Jones <gjones2023@chadwickschool.org>
2021-12-13 20:16:59 -0800

    docs: Change comments for MyAgentTest and MyAgent
    This change is being made to finish the project

--- src/main/java/MyAgent.java
+++ src/main/java/MyAgent.java
@@ -1,45 +1,51 @@
 import java.util.Random;
 
+
+
 /**
+
+ * Represents an agents movement strategy in the game including starting in the
+
+ * center of the board and moving outwards unless it is a bad move, a move
+
+ * allowing the opponent to win, not allowing the opponent to get three in a
+
+ * row, and lastly if nothing else works, simply place the agent in a spot that
+
+ * is not a bad move and the column is not full
+
  * 
- * Represents an agent's movement strategy in the game Connect4 by providing the
- * 
- * best starting location, moves following the initial one, defensive movements,
- * 
- * when the agent will win, when the opponent will win, when a move is bad, and
- * 
- * random moves.
- * 
- * 
- * 
+
  * @author gabrieljones
+
  *
- * 
- * 
+
  */
 
 public class MyAgent extends Agent {
 
 	/**
-	 * 
+
 	 * A random number generator to randomly decide where to place a token.
-	 * 
+
 	 */
 
 	private Random random;
 
+
+
 	/**
-	 * 
+
 	 * Constructs a new agent, giving it the game and telling it whether it is Red
-	 * 
+
 	 * or Yellow.
+
 	 *
-	 * 
-	 * 
+
 	 * @param game   The game the agent will be playing.
-	 * 
+
 	 * @param iAmRed True if the agent is Red, False if the agent is Yellow.
-	 * 
+
 	 */
 
 	public MyAgent(Connect4Game game, boolean iAmRed) {
@@ -50,54 +56,54 @@ public class MyAgent extends Agent {
 
 	}
 
-//CODE WASN'T GIVEN HERE SO COMMENT?
+
 
 	/**
-	 * 
+
 	 * The move method is run every time it is this agent's turn in the game. You
-	 * 
+
 	 * may assume that when move() is called, the game has at least one open slot
-	 * 
+
 	 * for a token, and the game has not already been won.
+
 	 *
-	 * 
-	 * 
+
 	 * <p>
-	 * 
+
 	 * By the end of the move method, the agent should have placed one token into
-	 * 
+
 	 * the game at some point.
-	 * 
+
 	 * </p>
+
 	 *
-	 * 
-	 * 
+
 	 * <p>
-	 * 
+
 	 * After the move() method is called, the game engine will check to make sure
-	 * 
+
 	 * the move was valid. A move might be invalid if: - No token was place into the
-	 * 
+
 	 * game. - More than one token was placed into the game. - A previous token was
-	 * 
+
 	 * removed from the game. - The color of a previous token was changed. - There
-	 * 
+
 	 * are empty spaces below where the token was placed.
-	 * 
+
 	 * </p>
+
 	 *
-	 * 
-	 * 
+
 	 * <p>
-	 * 
+
 	 * If an invalid move is made, the game engine will announce it and the game
-	 * 
+
 	 * will be ended.
-	 * 
+
 	 * </p>
+
 	 *
-	 * 
-	 * 
+
 	 */
 
 	public void move() {
@@ -124,6 +130,8 @@ public class MyAgent extends Agent {
 
 		}
 
+
+
 		else if (twoRowColumn != -1) {
 
 			moveOnColumn(twoRowColumn);
@@ -144,16 +152,18 @@ public class MyAgent extends Agent {
 
 	}
 
+
+
 	/**
-	 * 
+
 	 * Drops a token into a particular column so that it will fall to the bottom of
-	 * 
+
 	 * the column. If the column is already full, nothing will change.
+
 	 *
-	 * 
-	 * 
+
 	 * @param columnNumber The column into which to drop the token.
-	 * 
+
 	 */
 
 	public void moveOnColumn(int columnNumber) {
@@ -188,18 +198,20 @@ public class MyAgent extends Agent {
 
 	}
 
+
+
 	/**
-	 * 
+
 	 * Returns the index of the top empty slot in a particular column.
+
 	 *
-	 * 
-	 * 
+
 	 * @param column The column to check.
-	 * 
+
 	 * @return the index of the top empty slot in a particular column; -1 if the
-	 * 
+
 	 *         column is already full.
-	 * 
+
 	 */
 
 	public int getLowestEmptyIndex(Connect4Column column) {
@@ -220,16 +232,18 @@ public class MyAgent extends Agent {
 
 	}
 
+
+
 	/**
-	 * 
+
 	 * Returns a random valid move. If your agent doesn't know what to do, making a
-	 * 
+
 	 * random move can allow the game to go on anyway.
+
 	 *
-	 * 
-	 * 
+
 	 * @return a random valid move.
-	 * 
+
 	 */
 
 	public int randomMove() {
@@ -246,28 +260,28 @@ public class MyAgent extends Agent {
 
 	}
 
-//NO CODE HERE ORIGINALLY COMMENT?
+
 
 	/**
-	 * 
+
 	 * Returns the column that would allow the agent to win.
+
 	 *
-	 * 
-	 * 
+
 	 * <p>
-	 * 
+
 	 * You might want your agent to check to see if it has a winning move available
-	 * 
+
 	 * to it so that it can go ahead and make that move. Implement this method to
-	 * 
+
 	 * return what column would allow the agent to win.
-	 * 
+
 	 * </p>
+
 	 *
-	 * 
-	 * 
+
 	 * @return the column that would allow the agent to win.
-	 * 
+
 	 */
 
 	public int iCanWin() {
@@ -302,28 +316,28 @@ public class MyAgent extends Agent {
 
 	}
 
-	// NO CODE HERE ORIGINALLY COMMENT?
+
 
 	/**
-	 * 
+
 	 * Returns the column that would allow the opponent to win.
+
 	 *
-	 * 
-	 * 
+
 	 * <p>
-	 * 
+
 	 * You might want your agent to check to see if the opponent would have any
-	 * 
+
 	 * winning moves available so your agent can block them. Implement this method
-	 * 
+
 	 * to return what column should be blocked to prevent the opponent from winning.
-	 * 
+
 	 * </p>
+
 	 *
-	 * 
-	 * 
+
 	 * @return the column that would allow the opponent to win.
-	 * 
+
 	 */
 
 	public int theyCanWin() {
@@ -356,14 +370,16 @@ public class MyAgent extends Agent {
 
 	}
 
+
+
 	/**
-	 * 
+
 	 * Returns the name of this agent.
+
 	 *
-	 * 
-	 * 
+
 	 * @return the agent's name
-	 * 
+
 	 */
 
 	public String getName() {
@@ -372,22 +388,24 @@ public class MyAgent extends Agent {
 
 	}
 
+
+
 	/**
-	 * 
+
 	 * Returns <true> the index of the column where the opponent would win on the
-	 * 
+
 	 * next move if the agent moved on the specified column else return <false>
-	 * 
+
 	 * index values
+
 	 * 
+
 	 * 
-	 * 
-	 * 
-	 * 
+
 	 * @return <true> if the opponent would win on the next move if the agent moved
-	 * 
+
 	 *         on the specified column else return <false> index values
-	 * 
+
 	 */
 
 	public boolean[] isBadMove() {
@@ -422,20 +440,24 @@ public class MyAgent extends Agent {
 
 		return columnNotToMoveOn;
 
+
+
 	}
 
+
+
 	/**
-	 * 
+
 	 * Returns the column the agent should move on starting at the center column and
-	 * 
+
 	 * working outwards unless it is a bad move
+
 	 * 
-	 * 
-	 * 
+
 	 * @return what column the agent should move on starting in the middle and
-	 * 
+
 	 *         working outwards
-	 * 
+
 	 */
 
 	public int basicStrategy() {
@@ -480,20 +502,22 @@ public class MyAgent extends Agent {
 
 	}
 
+
+
 	/**
-	 * 
+
 	 * Returns the column, starting at index 0 and incrementing by 1, the agent
-	 * 
+
 	 * should move on if it is not a bad move and the column is not full
+
 	 * 
+
 	 * 
-	 * 
-	 * 
-	 * 
+
 	 * @return what column starting at index 0 and incrementing by 1, the agent
-	 * 
+
 	 *         should move on if it is not a bad move and the column is not full
-	 * 
+
 	 */
 
 	public int decentMove() {
@@ -514,43 +538,75 @@ public class MyAgent extends Agent {
 
 	}
 
+
+
 	/**
-	 * 
+
 	 * Returns the column, closest to index 0, the agent should move on if the
-	 * 
+
 	 * opponent has moved two spots in a row
+
 	 * 
-	 * 
-	 * 
+
 	 * @return what column, closest to index 0, the agent should move on if the
-	 * 
+
 	 *         opponent has moved two spots in a row
-	 * 
+
 	 */
+
 	public int twoInARow() {
+
 		char[][] board = myGame.getBoardMatrix();
+
 		boolean[] badMoveColumn = isBadMove();
+
 		if (iAmRed) {
+
 			for (int i = board.length - 1; i >= 0; i--) {
+
 				for (int j = 1; j < board[i].length - 3; j++) {
+
 					if ((board[i][j - 1] == 'B' && board[i][j] == 'Y' && board[i][j + 1] == 'Y'
+
 							&& board[i][j + 2] == 'B') && !badMoveColumn[j - 1]) {
+
 						return j - 1;
+
 					}
 
+
+
 				}
+
 			}
+
 		} else if (!iAmRed) {
+
 			for (int i = board.length - 1; i >= 0; i--) {
+
 				for (int j = 1; j < board[i].length - 3; j++) {
+
 					if ((board[i][j - 1] == 'B' && board[i][j] == 'R' && board[i][j + 1] == 'R'
+
 							&& board[i][j + 2] == 'B') && !badMoveColumn[j - 1]) {
+
 						return j - 1;
+
 					}
 
+
+
 				}
+
 			}
+
 		}
+
 		return -1;
+
 	}
+
 }
+
+
+
